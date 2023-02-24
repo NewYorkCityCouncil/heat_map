5185e52eb23de2bcf9db3c22b6aaa79fbf17cf37
Anne Driscoll <driscollannem@gmail.com>
2023-02-24 11:40:08 -0500

    add mean and median extract to gee

--- code/01_gee_get_mean_temp.js
+++ code/01_gee_get_mean_temp.js
@@ -48,180 +48,38 @@ print(landsat8.size())
 
 
 // -----------------------------------------------------------------------------
-// get means for entire period and by year for exporting -----------------------
+// get means for entire period and exporting -----------------------------------
+
 
 // get overall mean
-var heat_overall = landsat8
+var mean_heat = landsat8
                   .select('ST_B10')
                   .reduce(ee.Reducer.mean())
                   .clip(nyc);
                   
-var visParams = {min: 45500, max: 48500, palette: palettes.colorbrewer.Spectral[9].reverse()};
-Map.addLayer(heat_overall, visParams, "overall mean temp");
-                              
+var visParams = {min: 45000, max: 50000, palette: palettes.colorbrewer.Spectral[9].reverse()};
+Map.addLayer(mean_heat, visParams, "overall mean temp");
+        
 Export.image.toDrive({
-  image: heat_overall,
+  image: mean_heat,
   scale: 30,
-  description: 'surfacetemperature_2014_2022',
-  fileFormat: 'GeoTIFF',
-});
-
-// get the mean for 2014
-var heat_2014 = landsat8
-                  .filterDate('2014-01-01', '2014-12-31')
-                  .select('ST_B10')
-                  .reduce(ee.Reducer.mean())
-                  .clip(nyc)
-                  .subtract(heat_overall);
-
-var visParams = {min: -800, max: 800, palette: palettes.colorbrewer.Spectral[9].reverse()};
-//Map.addLayer(heat_2014.select('ST_B10_mean'), visParams, "2014 diff temp");
-                              
-                              
-Export.image.toDrive({
-  image: heat_2014,
-  scale: 30,
-  description: 'surfacetemperature_summer2014',
+  description: 'surfacetemperature_mean_2014_2022',
   fileFormat: 'GeoTIFF',
 });
 
 
-// get the mean for 2015
-var heat_2015 = landsat8
-                  .filterDate('2015-01-01', '2015-12-31')
+// get overall mean
+var median_heat = landsat8
                   .select('ST_B10')
-                  .reduce(ee.Reducer.mean())
-                  .clip(nyc)
-                  .subtract(heat_overall);
-
-//Map.addLayer(heat_2015.select('ST_B10_mean'), visParams, "2015 diff temp");
-                              
-Export.image.toDrive({
-  image: heat_2015,
-  scale: 30,
-  description: 'surfacetemperature_summer2015',
-  fileFormat: 'GeoTIFF',
-});
-
-
-// get the mean for 2016
-var heat_2016 = landsat8
-                  .filterDate('2016-01-01', '2016-12-31')
-                  .select('ST_B10')
-                  .reduce(ee.Reducer.mean())
-                  .clip(nyc)
-                  .subtract(heat_overall);
-
-//Map.addLayer(heat_2016.select('ST_B10_mean'), visParams, "2016 diff temp");
-                              
-Export.image.toDrive({
-  image: heat_2016,
-  scale: 30,
-  description: 'surfacetemperature_summer2016',
-  fileFormat: 'GeoTIFF',
-});
-
-
-// get the mean for 2017
-var heat_2017 = landsat8
-                  .filterDate('2017-01-01', '2017-12-31')
-                  .select('ST_B10')
-                  .reduce(ee.Reducer.mean())
-                  .clip(nyc)
-                  .subtract(heat_overall);
-
-//Map.addLayer(heat_2017.select('ST_B10_mean'), visParams, "2017 diff temp");
-                              
-Export.image.toDrive({
-  image: heat_2017,
-  scale: 30,
-  description: 'surfacetemperature_summer2017',
-  fileFormat: 'GeoTIFF',
-});
-
-
-// get the mean for 2018
-var heat_2018 = landsat8
-                  .filterDate('2018-01-01', '2018-12-31')
-                  .select('ST_B10')
-                  .reduce(ee.Reducer.mean())
-                  .clip(nyc)
-                  .subtract(heat_overall);
-
-//Map.addLayer(heat_2018.select('ST_B10_mean'), visParams, "2018 diff temp");
-                              
-Export.image.toDrive({
-  image: heat_2018,
-  scale: 30,
-  description: 'surfacetemperature_summer2018',
-  fileFormat: 'GeoTIFF',
-});
-
-
-// get the mean for 2019
-var heat_2019 = landsat8
-                  .filterDate('2019-01-01', '2019-12-31')
-                  .select('ST_B10')
-                  .reduce(ee.Reducer.mean())
-                  .clip(nyc)
-                  .subtract(heat_overall);
+                  .reduce(ee.Reducer.median())
+                  .clip(nyc);
                   
-//Map.addLayer(heat_2019.select('ST_B10_mean'), visParams, "2019 diff temp");
-                              
+var visParams = {min: 45000, max: 50000, palette: palettes.colorbrewer.Spectral[9].reverse()};
+Map.addLayer(median_heat, visParams, "overall median temp");
+          
 Export.image.toDrive({
-  image: heat_2019,
+  image: median_heat,
   scale: 30,
-  description: 'surfacetemperature_summer2019',
-  fileFormat: 'GeoTIFF',
-});
-
-// get the mean for 2020
-var heat_2020 = landsat8
-                  .filterDate('2020-01-01', '2020-12-31')
-                  .select('ST_B10')
-                  .reduce(ee.Reducer.mean())
-                  .clip(nyc)
-                  .subtract(heat_overall);
-                  
-//Map.addLayer(heat_2020.select('ST_B10_mean'), visParams, "2020 diff temp");
-                              
-Export.image.toDrive({
-  image: heat_2020,
-  scale: 30,
-  description: 'surfacetemperature_summer2020',
-  fileFormat: 'GeoTIFF',
-});
-
-// get the mean for 2021
-var heat_2021 = landsat8
-                  .filterDate('2021-01-01', '2021-12-31')
-                  .select('ST_B10')
-                  .reduce(ee.Reducer.mean())
-                  .clip(nyc)
-                  .subtract(heat_overall);
-                  
-//Map.addLayer(heat_2021.select('ST_B10_mean'), visParams, "2021 diff temp");
-                              
-Export.image.toDrive({
-  image: heat_2021,
-  scale: 30,
-  description: 'surfacetemperature_summer2021',
-  fileFormat: 'GeoTIFF',
-});
-
-// get the mean for 2022
-var heat_2022 = landsat8
-                  .filterDate('2022-01-01', '2022-12-31')
-                  .select('ST_B10')
-                  .reduce(ee.Reducer.mean())
-                  .clip(nyc)
-                  .subtract(heat_overall);
-                  
-//Map.addLayer(heat_2022.select('ST_B10_mean'), visParams, "2022 diff temp");
-                              
-Export.image.toDrive({
-  image: heat_2022,
-  scale: 30,
-  description: 'surfacetemperature_summer2022',
+  description: 'surfacetemperature_median_2014_2022',
   fileFormat: 'GeoTIFF',
 });
